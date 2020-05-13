f8f40f6770b82362fb24f899451f8ee46353a4d9
G2Gmycatisonfire <49059319+G2Gmycatisonfire@users.noreply.github.com>
2020-05-14 02:39:14 +0300

    Path Strings

--- app/src/main/java/com/example/ipapp/utils/UtilsSharedPreferences.java
+++ app/src/main/java/com/example/ipapp/utils/UtilsSharedPreferences.java
@@ -3,10 +3,16 @@ package com.example.ipapp.utils;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.util.Objects;
+
 public class UtilsSharedPreferences {
     private final static String PREFERENCE_FILE_KEY = "SPECTER_PREFERENCES";
     public final static String KEY_LOGGED_EMAIL = "EMAIL";
     public final static String KEY_LOGGED_PASSWORD = "PASSWORD";
+
+    public static boolean isUserLoggedIn(Context context){
+        return !context.getSharedPreferences(PREFERENCE_FILE_KEY, Context.MODE_PRIVATE).getString(KEY_LOGGED_EMAIL, "").isEmpty();
+    }
 
     //<editor-fold desc="GET AND SET VALUE">
     public static String getString(Context context, String key, String defaultValue) {
