cef1ae8ce57e3b676aa6e6406c11926a9a0e408e
Laurentiu Asan <lau.lau95@yahoo.com>
2020-05-25 00:47:02 +0300

    Create Document

--- app/src/main/java/com/example/ipapp/ui/documents/CreateDocumentActivity.java
+++ app/src/main/java/com/example/ipapp/ui/documents/CreateDocumentActivity.java
@@ -1,7 +1,18 @@
 package com.example.ipapp.ui.documents;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ipapp.R;
 
 public class CreateDocumentActivity extends AppCompatActivity {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_document);
+    }
 }
