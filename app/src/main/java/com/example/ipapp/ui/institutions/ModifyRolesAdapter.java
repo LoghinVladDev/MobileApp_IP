eb9dd62ad38f4ff91c1f9a1aaf831c85fa94c653
rares chelariu <rareschelariu79@gmail.com>
2020-05-25 20:57:01 +0300

    ViewHolder ctor made

--- app/src/main/java/com/example/ipapp/ui/institutions/ModifyRolesAdapter.java
+++ app/src/main/java/com/example/ipapp/ui/institutions/ModifyRolesAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,7 +19,7 @@ public class ModifyRolesAdapter extends RecyclerView.Adapter<ModifyRolesAdapter.
     private List<Role> mData;
     private LayoutInflater mInflater;
 
-    public ModifyRolesAdapter(Context context, List<Role> mData) {
+    ModifyRolesAdapter(Context context, List<Role> mData) {
         // TODO FINISH THIS
         this.mData = mData;
         this.mInflater = LayoutInflater.from(context);
@@ -43,12 +44,50 @@ public class ModifyRolesAdapter extends RecyclerView.Adapter<ModifyRolesAdapter.
         return mData.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        // TODO FINISH THIS
-        public TextView textViewRoleName;
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewRoleName;
+        Switch switchCanAddMembers;
+        Switch switchCanAddRoles;
+        Switch switchCanAssignRoles;
+        Switch switchCanDeassignRoles;
+        Switch switchCanDeleteInstitution;
+        Switch switchCanModifyRoles;
+        Switch switchCanModifyInstitution;
+        Switch switchCanRemoveMembers;
+        Switch switchCanUploadMembers;
+        Switch switchCanPreviewUploadedDocuments;
+        Switch switchCanRemoveUploadedDocuments;
+        Switch switchCanSendDocuments;
+        Switch switchCanPreviewReceivedDocuments;
+        Switch switchCanPreviewSpecificReceivedDocument;
+        Switch switchCanRemoveReceivedDocument;
+        Switch switchCanDownloadDocuments;
+        Switch switchCanRemoveRoles;
 
-        public ViewHolder(@NonNull View itemView) {
+        ViewHolder(@NonNull View itemView) {
             super(itemView);
+            switchCanModifyInstitution = itemView.findViewById(R.id.switchCanModifyInstitution);
+            switchCanDeleteInstitution = itemView.findViewById(R.id.switchCanDeleteInstitution);
+            switchCanAddMembers = itemView.findViewById(R.id.switchCanAddMembers);
+            switchCanDeleteInstitution = itemView.findViewById(R.id.switchCanDeleteInstitution);
+            switchCanAddMembers = itemView.findViewById(R.id.switchCanAddMembers);
+            switchCanRemoveMembers = itemView.findViewById(R.id.switchCanRemoveMembers);
+            switchCanUploadMembers = itemView.findViewById(R.id.switchCanUploadMembers);
+            switchCanPreviewUploadedDocuments = itemView.findViewById(R.id.switchCanPreviewUploadedDocuments);
+            switchCanRemoveUploadedDocuments = itemView.findViewById(R.id.switchCanRemoveUploadedDocuments);
+            switchCanSendDocuments = itemView.findViewById(R.id.switchCanSendDocuments);
+            switchCanPreviewReceivedDocuments = itemView.findViewById(R.id.switchCanPreviewReceivedDocuments);
+            switchCanPreviewSpecificReceivedDocument = itemView.findViewById(R.id.switchCanPreviewSpecificReceivedDocument);
+
+            switchCanRemoveReceivedDocument = itemView.findViewById(R.id.switchCanRemoveReceivedDocument);
+            switchCanDownloadDocuments = itemView.findViewById(R.id.switchCanDownloadDocuments);
+            switchCanAddRoles = itemView.findViewById(R.id.switchCanAddRoles);
+            switchCanRemoveRoles = itemView.findViewById(R.id.switchCanRemoveRoles);
+            switchCanModifyRoles = itemView.findViewById(R.id.switchCanModifyRoles);
+            switchCanAssignRoles = itemView.findViewById(R.id.switchCanAssignRoles);
+            switchCanDeassignRoles = itemView.findViewById(R.id.switchCanDeassignRoles);
+
+            switchCanModifyRoles = itemView.findViewById(R.id.switchCanModifyRoles);
 
         }
 
